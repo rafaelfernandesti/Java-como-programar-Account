ab6e37710677d6329e4f616c0241b45a6f9b5b10
Rafael Fernandes <rafa.fernan10@gmail.com>
2020-06-29 15:36:23 -0300

    Account com um atributo

---
+++ src/Account.java
@@ -0,0 +1,20 @@
+public class Account {
+    private String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    Account() {
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
