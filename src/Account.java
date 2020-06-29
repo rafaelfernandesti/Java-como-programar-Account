c25ad05ef3e5f9a2ce813b43cddd9804a1fc129d
Rafael Fernandes <rafa.fernan10@gmail.com>
2020-06-29 16:01:21 -0300

    Account com 2 atributos e construtor com validação

--- src/Account.java
+++ src/Account.java
@@ -1,13 +1,31 @@
 public class Account {
     private String name;
-
-    public Account(String name) {
+    private double balance;
+    
+    //construtor com validação. Recebe dois parâmetros.
+    public Account(String name, double initialBalance) {
         this.name = name;
+        
+        if(initialBalance > 0){ //valida se o valor é maior que 0.
+            this.balance = initialBalance;
+        }
+    }
+    
+    //método para depositar quantia no saldo
+    public void deposit(double depositAmount){
+        if(depositAmount>0){
+            balance = balance + depositAmount; //adiciona ao saldo
+        }
+    }
+    
+    
+    public double getBalance() {
+        return balance;
     }
 
-    Account() {
-        
-    }
+//    public void setBalance(double balance) {
+//        this.balance = balance;
+//    }
 
     public String getName() {
         return name;
--- src/AccountTest.java
+++ src/AccountTest.java
@@ -6,16 +6,33 @@ public class AccountTest {
     
     public static void main(String[] args)
     {
+        Account account1 = new Account("Jane Green", 50);
+        Account account2 = new Account("John Blue", -10.50);
+        
+        //exibe saldo inicial dos objetos
+        System.out.printf("%s Saldo da conta 1: %.2f %n",account1.getName(), account1.getBalance());
+        System.out.printf("%s Saldo da conta 2: %.2f %n",account2.getName(),account2.getBalance());
+        
+        //cria um Scanner para a entrada dos dados na linha de comandos
         Scanner input = new Scanner(System.in);
-        Account myAccount = new Account();
-        System.out.printf("Initial name is %s%n%n", myAccount.getName());
         
-        System.out.println("Please enter the name: ");
-        String theName = input.nextLine();
-        myAccount.setName(theName);
-        System.out.println("");
+        System.out.println("Digite o valor do depósito para a Conta 1: ");
+        double depositAmount = input.nextDouble();
+        System.out.printf("%nAdicionando %.2f ao saldo da Conta 1%n", depositAmount);
+        account1.deposit(depositAmount);
         
-        System.out.printf("Name in object myAccount is:%n%s%n", myAccount.getName());
+        //exibe os saldos das duas contas
+        System.out.printf("%s Saldo: %.2f %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s Saldo: %.2f %n", account2.getName(), account2.getBalance());
+        
+        System.out.println("Digite o valor do depósito para a Conta 2: ");
+        depositAmount = input.nextDouble();
+        System.out.printf("%nAdicionando %.2f ao saldo da Conta 2%n", depositAmount);
+        account2.deposit(depositAmount);
+        
+        System.out.printf("%s Saldo: %.2f %n", account1.getName(), account1.getBalance());
+        System.out.printf("%s Saldo: %.2f %n", account2.getName(), account2.getBalance());
+        
     }
     
 }
